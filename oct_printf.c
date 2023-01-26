bdd7b52cebac85c37dd42a1ab09fcc4d25c389e1
Dennis Muvaa <dmuvaa70@gmail.com>
2023-01-26 03:24:57 -0800

    Printf, here we go

---
+++ oct_printf.c
@@ -0,0 +1,21 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * print_oct - prints number
+ * @num: number to be printed
+ *
+ * Return: void
+ */
+
+int print_oct(unsigned int num)
+{
+	unsigned int count;
+
+	count = 0;
+	if (num / 8)
+		count += print_oct(num / 8);
+
+	count += _putchar((num % 8) + '0');
+	return (count);
+}
